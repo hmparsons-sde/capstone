86dc29fe630ce6fe0c7cb26ed039e82bc213526e
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-05-31 17:15:10 -0500

    install packages + add db

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -1,7 +1,9 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  localApiKey: process.env.REACT_APP_API_KEY_LOCAL,
+  externalApiKey: process.env.REACT_APP_API_KEY_EXTERNAL,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  localDbURL: process.env.REACT_APP_DATABASE_URL_LOCAL,
+  externalDbURL: process.env.REACT_APP_DATABASE_URL_EXTERNAL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import firebase from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebaseConfig from './helpers/apiKeys';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.Fragment>
