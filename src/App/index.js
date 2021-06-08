5af35e990310afd03ff13b63f8dc03b43f6127b7
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-06-07 19:04:41 -0500

    read, create, delete working

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import firebase from 'firebase';
-import NavBar from '../Components/Nav/NavBar';
+import NavBar from '../Components/Nav/Navbar';
 // import PressureView from '../views/SearchView';
 import Routes from '../helpers/Routes';
 
 function App() {
+  const [trips, setTrips] = useState([]);
+
   return (
     <div className='App'>
       <Router>
         <NavBar/>
-        <Routes/>
+        <Routes
+          trips={trips}
+          setTrips={setTrips}
+        />
       </Router>
     </div>
   );
--- src/Components/Cards/TripCard.js
+++ src/Components/Cards/TripCard.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+  Button,
+  Card,
+  CardText,
+  CardTitle
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import { deleteTrip } from '../../helpers/data/tripData';
 
-export default function TripCard() {
+const TripCard = ({
+  firebaseKey,
+  title,
+  startDate,
+  endDate,
+  setTrips
+}) => {
+  const handleClick = () => {
+    deleteTrip(firebaseKey)
+      .then((tripsArray) => setTrips(tripsArray));
+  };
+
   return (
-    <div>
-      <h1>Not Found</h1>
-    </div>
+    <Card body>
+        <CardTitle tag="h5">{title}</CardTitle>
+        <CardText>{startDate} - {endDate}</CardText>
+        <Button color="danger" onClick={handleClick}>Delete Trip</Button>
+    </Card>
   );
-}
+};
+
+TripCard.propTypes = {
+  firebaseKey: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  endDate: PropTypes.string,
+  startDate: PropTypes.string,
+  setTrips: PropTypes.func
+};
+
+export default TripCard;
