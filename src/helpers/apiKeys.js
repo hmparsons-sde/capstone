90c592a6f19b7eae99ff7f65f1b8caf9d07d77d6
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-06-08 20:30:02 -0500

    corrected config

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -1,9 +1,7 @@
 const firebaseConfig = {
-  localApiKey: process.env.REACT_APP_API_KEY,
-  externalApiKey: process.env.REACT_APP_API_KEY_EXTERNAL,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  localDbURL: process.env.REACT_APP_DATABASE_URL_LOCAL,
-  externalDbURL: process.env.REACT_APP_DATABASE_URL_EXTERNAL,
+  dbURL: process.env.REACT_APP_DATABASE_URL_LOCAL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
