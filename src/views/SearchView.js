7980c18e3302e8a8283f23cbf84504e9d51b4b18
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-06-12 18:12:30 -0500

    form resets + form appears on click

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form } from 'reactstrap';
+import { Button, Form, Input } from 'reactstrap';
 import SearchResultCard from '../Components/Cards/SearchResultCard';
 import { getPressureData } from '../helpers/data/externalData';
 
-export default function PublicSearchView({ firebaseKey, uid }) {
+export default function SearchResultView({ firebaseKey, uid }) {
   const [pressure, setPressure] = useState([]);
   const [userInput, setUserInput] = useState('');
+  const [showButton, setShowButton] = useState(false);
+
+  const handleClick = () => {
+    setShowButton((prevState) => !prevState);
+  };
+
   const grabPressure = () => {
     getPressureData(userInput)
       .then((response) => {
@@ -15,43 +21,51 @@ export default function PublicSearchView({ firebaseKey, uid }) {
       });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setUserInput('');
-    grabPressure();
-  };
-
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    grabPressure();
+    setUserInput('');
+  };
+
+  const resetSearchResults = () => {
+    setPressure([]);
+  };
+
   return (
     <div>
-      <Form
-        onSubmit={handleSubmit}
-        id="get-Pressure"
-        className="shadow p-3 rounded">
-          <div className="form-group">
-            <h2
-              id="search-title">
-                Get the Pressure
-            </h2>
-            <input
-              type="text"
-              className="form-control"
-              id="value"
-              aria-describedby="location"
-              onChange={handleUserInput}>
-            </input>
-          </div>
-        <Button
-          type="submit"
-          id="search-Pressure"
-          outline color="dark">
-            Submit
-        </Button>
-      </Form>
+      { !showButton ? <Button className="m-2 btn-lg justify-content-center" color='danger' onClick={handleClick}>Search</Button>
+        : <Form
+          onSubmit={handleSubmit}
+          id="get-Pressure"
+          className="shadow p-3 rounded">
+            <div className="form-group">
+              <h2
+                id="search-title">
+                  Get the Pressure
+              </h2>
+              <Input
+                type="text"
+                className="form-control"
+                id="value"
+                value={userInput}
+                aria-describedby="location"
+                onChange={handleUserInput}>
+              </Input>
+            </div>
+          <Button
+            type="submit"
+            id="search-Pressure"
+            outline color="dark">
+              Submit
+          </Button>
+        </Form>
+      }
       <div id="card-container">
+        {pressure !== [] && <Button onClick={resetSearchResults}>Clear</Button>}
         {pressure.map((pressureObj) => (
           <SearchResultCard
             key={pressureObj.id}
@@ -65,7 +79,7 @@ export default function PublicSearchView({ firebaseKey, uid }) {
   );
 }
 
-PublicSearchView.propTypes = {
+SearchResultView.propTypes = {
   firebaseKey: PropTypes.any,
   uid: PropTypes.any
 };
